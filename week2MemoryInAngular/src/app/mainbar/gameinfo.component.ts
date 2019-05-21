fbd63312f6a78e83182bbdfc21e57f4e90ecdff6
Vodash <k.udema@st.hanze.nl>
2019-05-21 13:42:27 +0200

    added timer to game service for mainbar

--- week2MemoryInAngular/src/app/mainbar/gameinfo.component.ts
+++ week2MemoryInAngular/src/app/mainbar/gameinfo.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import {interval, Subscription} from "rxjs";
 
 @Component({
   selector: 'gameinfo',
   template: `
     <h2>Javascript memory</h2>
-    <div>Verlopen tijd: <span id="tijd">0</span> seconden.<br/>
+    <div>Verlopen tijd: <span id="tijd">{{returnTime()}}</span> seconden.<br/>
     Gevonden kaart-paren: <span id="gevonden">0</span><br>
     </div>
   `
 })
-export class GameinfoComponent { }
+export class GameinfoComponent {
+  subscription: Subscription;
+  currTime: number = 0;
+
+  ngOnInit(){
+    const source = interval(1000);
+    this.subscription = source.subscribe(val => this.timerAdd());
+  }
+
+  timerAdd(){
+    this.currTime = this.currTime + 1;
+  }
+
+  returnTime(){
+    return this.currTime
+  }
+
+}
