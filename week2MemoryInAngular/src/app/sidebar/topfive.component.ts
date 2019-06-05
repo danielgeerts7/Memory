54c805d326386f2f45342c839ac3c5d297f0cadf
Florian Molenaars <f.molenaars@hotmail.com>
2019-06-05 15:02:54 +0200

    done

--- week2MemoryInAngular/src/app/sidebar/topfive.component.ts
+++ week2MemoryInAngular/src/app/sidebar/topfive.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'topfive',
   template: `
+      <button class = "logout" (click)= logout()>Logout</button>
       <b>Top vijf</b>
       <ol id="topscores"></ol>
   `
 })
-export class TopfiveComponent { }
+export class TopfiveComponent {
+
+  constructor(private authService: AuthService, private router: Router){
+
+  }
+  logout(){
+    this.authService.logout();
+    this.router.navigateByUrl('/auth/login');
+
+  }
+}
